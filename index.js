c809223d8124ad9e12ba56b3e425804b621f0f75
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-10 00:14:05 -0300

    importamos los middleware

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 import express from 'express'
+import cors from 'cors'
+import express from 'express'
+
+
 const app = express()
 const PORT = 3000
+
+app.use(cors)
 
 console.log('mensaje de prueba')
 
