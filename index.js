77373ca95b1040e86456a65c3581fcc056dbe430
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-09 23:50:33 -0300

    agregamos en start node index

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+
+import express from 'express'
+const app = express()
+const PORT = 3000
+
 console.log('mensaje de prueba')
+
+app.listen(PORT, () => {console.info (`servidor activo en el puerto ${PORT}`)})
